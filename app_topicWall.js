4bb3292a005f7c7b6c19c7eced9dd32a393f16ca
b8208241 <b8208241@gmail.com>
2016-11-18 13:29:52 +0800

    修改: 可以載入上次結果、可以新增磚頭、新增時可以使用新增按鈕、新增後結果也能儲存

--- app_topicWall.js
+++ app_topicWall.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import TopicPage from '../views/topic_Wall.jsx';
 
 let serverRenderRow = document.getElementById('rowOne').innerHTML;
-ReactDOM.render(<TopicPage rowRecord = {serverRenderRow}/>, document.getElementById("app"));
-
+ReactDOM.render(<TopicPage rowRecordOne = {serverRenderRow}/>, document.getElementById("app"));
