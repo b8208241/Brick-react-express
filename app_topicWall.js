2e1fcaaa6b143448e4aba73d17547e5155e90367
b8208241 <b8208241@gmail.com>
2016-11-26 23:43:27 +0800

    Delete app_topicWall.js

--- app_topicWall.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TopicPage from '../views/topic_Wall.jsx';
-
-let serverRenderRow = document.getElementById('rowOne').innerHTML;
-ReactDOM.render(<TopicPage rowRecordOne = {serverRenderRow}/>, document.getElementById("app"));
